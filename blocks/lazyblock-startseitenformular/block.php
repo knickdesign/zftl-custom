0e517754db5e03f0070376d4cc9427bcca2d49e9
les-knick <lea.stocker@knickdesign.de>
2022-03-22 17:33:57 +0100

    slider img

--- blocks/lazyblock-startseitenformular/block.php
+++ blocks/lazyblock-startseitenformular/block.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Banner Swoosh Block Template.
  *
@@ -11,32 +12,32 @@
 <div class="startseitenformular">
     <h2 class="text-super">Machen Sie den Fitness-Schnellcheck</h2>
     <form action="<?php echo get_home_url() ?>/wp-admin/admin-ajax.php" method="POST" id="startseitenformular" class="startseitenformular__form">
-    <!-- Filterkategorie -->
+        <!-- Filterkategorie -->
         <h3>Woran möchten Sie arbeiten?</h3>
         <div class="startseitenformular__form__button-wrapper flex">
             <!-- Filter -->
             <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="problem" value="ubergewicht" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl_uebergewicht.svg">
                     <h4>Über&shy;gewicht</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
             </label>
-             <!-- Filter -->
-             <label class="startseitenformular__form__button-wrapper__button">
+            <!-- Filter -->
+            <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="problem" value="gelenkschmerzen" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl-rueckenschmerzen.svg">
                     <h4>Gelenk&shy;schmerzen</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
             </label>
-             <!-- Filter -->
-             <label class="startseitenformular__form__button-wrapper__button">
+            <!-- Filter -->
+            <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="problem" value="allgemeineFitness" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl-allgemeine_fitness.svg">
                     <h4>allgemeine Fitness</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
@@ -45,9 +46,37 @@
         <!-- Filterkategorie -->
         <h3>Seit wann besteht das Problem?</h3>
         <div class="startseitenformular__form__slider-wrapper">
-             <!-- Filter -->
-                <input type="range" name="dauer" min="0" max="100" value="50" class="slider" />
+            <img id="slider-img" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/20220316_zeitbubbles-3.svg">
+            <!-- Filter -->
+            <input type="range" name="dauer" min="0" max="3" value="1" class="slider" id="time-slider" />
         </div>
+        <script>
+            var slider = document.getElementById("time-slider");
+            var slider_img = document.getElementById("slider-img");
+            var output = 0;
+            output = slider.value;
+
+            slider.oninput = function() {
+                output = this.value;
+                console.log(output);
+                if(output == 0){
+                    slider_img.src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/20220316_zeitbubbles_4.svg";
+                    console.log("little success");
+                }
+                else if(output == 1){
+                    slider_img.src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/20220316_zeitbubbles-3.svg";
+                    console.log("little success");
+                }
+                else if(output == 2){
+                    slider_img.src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/20220316_zeitbubbles-2.svg";
+                    console.log("little success");
+                }
+                else if(output == 3){
+                    slider_img.src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/20220316_zeitbubbles-1.svg";
+                    console.log("little success");
+                }
+            }
+        </script>
         <!-- Filterkategorie -->
         <h3>Wie schwer ist ihr Leidensdruck?</h3>
         <div class="startseitenformular__form__button-wrapper flex">
@@ -55,36 +84,36 @@
             <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="leidensdruck" value="sehrGroß" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl-leidensdruck01.svg">
                     <h4>Sehr groß</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
             </label>
-             <!-- Filter -->
-             <label class="startseitenformular__form__button-wrapper__button">
+            <!-- Filter -->
+            <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="leidensdruck" value="mittelmassig" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl-leidensdruck02.svg">
                     <h4>Mittelmäßig</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
             </label>
-             <!-- Filter -->
-             <label class="startseitenformular__form__button-wrapper__button">
+            <!-- Filter -->
+            <label class="startseitenformular__form__button-wrapper__button">
                 <input type="radio" name="leidensdruck" value="kaum" />
                 <div class="startseitenformular__form__button-wrapper__button__inner">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/TMP-prev-button.png">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/illustrations/zftl-leidensdruck03.svg">
                     <h4>Kaum</h4>
                     <div class="startseitenformular__form__button-wrapper__button__inner__checkcircle"></div>
                 </div>
             </label>
-        </div> 
+        </div>
         <div class="startseitenformular__form__txt">
             <h2>Ein guter Anfang</h2>
             <p class="text-bigger">
                 Danke für den ersten Eindruck. Die Details besprechen wir gerne beim kostenlosen Beratungstermin. Ihre Fitness ist bei uns Maßarbeit! Ein paar Tipps haben wir schon jetzt für Sie:
             </p>
-        </div>                   
+        </div>
         <button class="startseitenformular__form__button">Jetzt Auswertung ansehen</button>
         <input type="hidden" name="action" value="myfilter">
     </form>
